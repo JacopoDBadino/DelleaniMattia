943b526ccaffa956fd8c0bfaf42bfdccf2a18908
mattia <mattia@DESKTOP-TSS6NL7>
2020-06-22 19:22:37 +0200

    prova

---
+++ src/main/java/it/polito/tdp/comprensorio_sciistico/Main.java
@@ -0,0 +1,7 @@
+package it.polito.tdp.comprensorio_sciistico;
+
+public class Main {
+	public static void main(String[] args) {
+        EntryPoint.main(args);
+    }
+}
---
+++ src/main/java/it/polito/tdp/comprensorio_sciistico/model/Model.java
@@ -0,0 +1,5 @@
+package it.polito.tdp.comprensorio_sciistico.model;
+
+public class Model {
+
+}
